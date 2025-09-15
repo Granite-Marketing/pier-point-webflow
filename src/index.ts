76b43c3f563f8598f7950c0a58b914c42ebca211
Stephen Anindo <stephen@granitemarketing.co.uk>
2025-09-15 17:17:41 +0100

    remove import from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import { accordion } from '$utils/accordion';
 import { gsapBasicAnimations } from '$utils/gsapBasicAnimations';
 import { gsapSmoothScroll } from '$utils/gsapSmoothScroll';
 import { swiperSliders } from '$utils/swiperSliders';
-import { gsap } from 'gsap';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   gsapSmoothScroll();
   accordion();
-  gsapBasicAnimations(gsap, ScrollTrigger);
+  gsapBasicAnimations();
   swiperSliders();
 });
