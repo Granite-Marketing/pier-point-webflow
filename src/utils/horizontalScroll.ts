08001cd02d35803099a29a39819f275a7752afbd
Abdulfettah <fettahaud@gmail.com>
2025-10-04 15:31:42 +0300

    stop the horizontal animation

--- src/utils/horizontalScroll.ts
+++ src/utils/horizontalScroll.ts
@@ -2,25 +2,29 @@ declare const gsap: any;
 declare const ScrollTrigger: any;
 
 export const horizontalScroll = () => {
-  const horizontalScrollSections = document.querySelectorAll('.section_h-scroll');
+  const horizontalScrollSections = document.querySelectorAll('.h-scroll_transition-wrap-wrap');
   if (horizontalScrollSections.length > 0) {
     horizontalScrollSections.forEach((section) => {
-      const scrollWrapper = section.querySelector('.h-scroll_collection')!;
+      const scrollWrapper = section.querySelector('.h-scroll_transition-wrap')!;
       const tl = gsap.timeline();
+      // gsap.set(scrollWrapper, {
+      //   width: 'max-content',
+      // });
+      console.log(scrollWrapper);
 
-      tl.to(section.querySelector('.h-scroll_component'), {
-        x: scrollWrapper.getBoundingClientRect().width * -1,
-        // ease: 'none',
-      });
-      ScrollTrigger.create({
-        trigger: section,
-        start: 'left left',
-        end: `+=${scrollWrapper.getBoundingClientRect().width}`,
-        scrub: true,
-        markers: false,
-        pin: true,
-        animation: tl,
-      });
+      // tl.to(scrollWrapper, {
+      //   x: scrollWrapper.getBoundingClientRect().width * -1,
+      //   // ease: 'none',
+      // });
+      // ScrollTrigger.create({
+      //   trigger: section,
+      //   start: 'left left',
+      //   end: `+=${scrollWrapper.getBoundingClientRect().width}`,
+      //   scrub: true,
+      //   markers: true,
+      //   pin: true,
+      //   animation: tl,
+      // });
     });
   }
 };
