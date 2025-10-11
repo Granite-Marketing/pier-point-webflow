dfb97472797c5f82fadc4c489bb9945a64f7ce98
Abdulfettah <fettahaud@gmail.com>
2025-10-11 13:04:01 +0300

    added mobile support for image scroll animation

--- src/utils/imagesScroll.ts
+++ src/utils/imagesScroll.ts
@@ -6,20 +6,38 @@ export const imagesScroll = () => {
   if (sections.length > 0) {
     sections.forEach((section) => {
       const images = section.querySelectorAll('.flashy-gallery_fig-2');
-
-      const tl = gsap.timeline();
-      tl.to(images, {
-        y: `-${images.length * 50}%`,
-        ease: 'none',
+      const mm = gsap.matchMedia();
+      mm.add('(min-width: 768px)', () => {
+        const tl = gsap.timeline();
+        tl.to(images, {
+          y: `-${images.length * 50}%`,
+          ease: 'none',
+        });
+        ScrollTrigger.create({
+          trigger: section,
+          markers: false,
+          pin: true,
+          scrub: true,
+          start: 'top top',
+          end: `+=${images.length * 50}%`,
+          animation: tl,
+        });
       });
-      ScrollTrigger.create({
-        trigger: section,
-        markers: false,
-        pin: true,
-        scrub: true,
-        start: 'top top',
-        end: '+=150%',
-        animation: tl,
+      mm.add('(max-width: 767px)', () => {
+        const tl = gsap.timeline();
+        tl.to(images, {
+          x: `-${images.length * 50}%`,
+          ease: 'none',
+        });
+        ScrollTrigger.create({
+          trigger: section,
+          markers: false,
+          pin: true,
+          scrub: true,
+          start: 'top top',
+          end: `+=${images.length * 50}%`,
+          animation: tl,
+        });
       });
     });
   }
