897b3237a55871118d3e1280a469cb4409328ba9
Abdulfettah <fettahaud@gmail.com>
2025-10-11 12:38:05 +0300

    Fix images scroll

--- src/utils/imagesScroll.ts
+++ src/utils/imagesScroll.ts
@@ -5,12 +5,12 @@ export const imagesScroll = () => {
   const sections = document.querySelectorAll('.section_flashy-gallery .flashy-gallery_component');
   if (sections.length > 0) {
     sections.forEach((section) => {
-      const images = section.querySelectorAll('.flashy-gallery_fig');
+      const images = section.querySelectorAll('.flashy-gallery_fig-2');
+
       const tl = gsap.timeline();
       tl.to(images, {
         y: `-${images.length * 50}%`,
         ease: 'none',
-        stagger: 'top',
       });
       ScrollTrigger.create({
         trigger: section,
@@ -18,7 +18,7 @@ export const imagesScroll = () => {
         pin: true,
         scrub: true,
         start: 'top top',
-        end: '+=200%',
+        end: '+=150%',
         animation: tl,
       });
     });
