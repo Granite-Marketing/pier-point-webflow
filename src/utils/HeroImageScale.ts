e7611b0178991fec67f94389da5054d785cd7cf6
Abdulfettah <fettahaud@gmail.com>
2025-10-23 01:10:05 +0300

    Updated video scale animations

--- src/utils/HeroImageScale.ts
+++ src/utils/HeroImageScale.ts
@@ -1,6 +1,7 @@
 declare const gsap: any;
 declare const ScrollTrigger: any;
 declare const Flip: any;
+declare const SplitText: any;
 
 export const heroImageAnimations = () => {
   //   const heroImage = document.querySelector('[hero-image-masker]:nth-child(3)'); // in general take random one
@@ -10,11 +11,14 @@ export const heroImageAnimations = () => {
   const newHeroImageHolder = document.querySelector(
     '.section_hero-home .hero-home_bg'
   ) as HTMLElement;
+  const sectionTitle = document.querySelector('.section_hero-home .hero-home_title');
+  const sectionDes = document.querySelector('.section_hero-home .hero-home_content p');
 
   if (heroImage && newHeroImageHolder) {
     // heroImage.style.cssText = 'position: relative; z-index: 100;';
     const state = Flip.getState(heroImage);
     newHeroImageHolder.appendChild(heroImage);
+    const mm = gsap.matchMedia();
 
     const heroImages = document.querySelectorAll(
       '[hero-image-masker] .hero-mask_fig'
@@ -44,6 +48,65 @@ export const heroImageAnimations = () => {
       });
     });
 
+    const textTl = gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: (i) => 1 - Math.pow(1 - i, 4),
+      },
+    });
+
+    mm.add('(min-width:768px)', () => {
+      gsap.set(sectionTitle, {
+        textWrap: 'nowrap',
+      });
+    });
+    mm.add('(max-width:768px)', () => {
+      gsap.set(sectionTitle, {
+        textWrap: 'wrap',
+      });
+    });
+
+    const titleSplit = new SplitText(sectionTitle, {
+      type: 'words,lines',
+      mask: 'lines',
+    });
+    const desSplit = new SplitText(sectionDes, {
+      type: 'words,lines',
+      mask: 'lines',
+    });
+
+    textTl
+      .fromTo(
+        newHeroImageHolder.querySelector('.u-overlay'),
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          duration: 0.5,
+        }
+      )
+      .fromTo(
+        titleSplit.lines,
+        {
+          yPercent: 100,
+        },
+        {
+          yPercent: 0,
+        },
+        '-=.25'
+      )
+      .fromTo(
+        desSplit.lines,
+        {
+          yPercent: 100,
+        },
+        {
+          yPercent: 0,
+        },
+        '-=.75'
+      );
+
     ScrollTrigger.create({
       trigger: newHeroImageHolder,
       start: 'top bottom',
@@ -58,5 +121,43 @@ export const heroImageAnimations = () => {
         scale: false,
       }),
     });
+    ScrollTrigger.create({
+      trigger: newHeroImageHolder,
+      start: 'top center',
+      end: 'bottom bottom',
+      scrub: true,
+      markers: false,
+      animation: gsap.to(heroImage, {
+        border: '0',
+      }),
+    });
+    ScrollTrigger.create({
+      trigger: newHeroImageHolder,
+      start: 'top top',
+      // end: 'bottom bottom',
+      // scrub: true,
+      markers: true,
+      animation: textTl,
+      toggleActions: 'play none play reverse',
+    });
+    // .fromTo(
+    //   titleSplit.lines,
+    //   {
+    //     yPercent: 100,
+    //   },
+    //   {
+    //     yPercent: 0,
+    //   }
+    // )
+    // .fromTo(
+    //   desSplit.lines,
+    //   {
+    //     yPercent: 100,
+    //   },
+    //   {
+    //     yPercent: 0,
+    //     stagger: 0.05,
+    //   }
+    // ),
   }
 };
