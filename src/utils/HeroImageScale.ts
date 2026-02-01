e1dbd505f153693d828647540058e82083814fbb
Abdulfettah <fettahaud@gmail.com>
2026-02-01 21:54:47 +0300

    fix overlay

--- src/utils/HeroImageScale.ts
+++ src/utils/HeroImageScale.ts
@@ -104,7 +104,7 @@ const flipVideoAnimation = () => {
 
   textTl
     .fromTo(
-      newHeroImageHolder.querySelector('.u-overlay'),
+      heroImage?.querySelector('.u-overlay'),
       {
         opacity: 0,
       },
